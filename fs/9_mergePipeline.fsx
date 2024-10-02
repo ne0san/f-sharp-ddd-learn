a0e92d64ef410614ebf69aa4318197d9603bf111
浅見 朋 <t-asami@techfirm.co.jp>
2024-10-02 09:47:48 +0900

    9-3、まで

--- fs/9_mergePipeline.fsx
+++ fs/9_mergePipeline.fsx
@@ -2,7 +2,7 @@ open System
 
 // 9-1
 module Domain =
-    type orderId = private OrderId of string
+    type OrderId = private OrderId of string
 
     module OrderId =
         let create str =
@@ -16,3 +16,45 @@ module Domain =
                 OrderId str
 
         let value (OrderId str) = str
+
+
+open Domain
+type Undefined = exn
+type AsyncResult<'success, 'failure> = Async<Result<'success, 'failure>>
+type UnvalidatedAddress = UnvalidatedAddress of string
+and OrderId = Undefined
+and CustomerInfo = Undefined
+and Address = Address of string
+type ValidatedOrderLine = ValidatedOrderLine of Undefined
+and UnvalidatedCustomer = { Name: string; Email: string }
+type UnvalidatedOrder =
+    { OrderId: string
+      CutomerInfo: UnvalidatedCustomer
+      ShippingAddress: UnvalidatedAddress }
+type ValidatedOrder =
+    { OrderId: OrderId
+      CustomerInfo: CustomerInfo
+      ShippingAddress: Address
+      BillingAddress: Address
+      OrderLine: ValidatedOrderLine list }
+type ProductCode =
+    | WidgetCode of WidgetCode
+    | GizmoCode of GizmoCode
+and WidgetCode = WidgetCode of string
+and GizmoCode = GizmoCode of string
+type CheckProductCodeExists = ProductCod
+type CheckAddress = CheckAddress of Address
+type PlacedOrderError = ValidationError of ValidationError list
+and ValidationError =
+    { FieldName: string
+      ErrorDescription: string }
+type AddressValidationError = AddressValidationError of string
+
+// ------------- 型定義ここまで
+
+type CheckAddressExists =
+    UnvalidatedAddress -> AsyncResult<CheckAddress, AddressValidationError>
+
+type ValidateOrder = 
+    CheckProductCodeExists -> CheckAddressExists -> UnvalidatedOrder ->AsyncResult<ValidatedOrder, ValidationError list>
+
