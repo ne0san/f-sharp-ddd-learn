d7d70e8bb51c8b0faa5fb6f3bdadeaaa1f665510
neosan <nilva3465@gmail.com>
2024-10-05 18:21:35 +0900

    むずいけどおもろすぎる

--- fs/10-3ErrorHandle.fsx
+++ fs/10-3ErrorHandle.fsx
@@ -1,30 +1,43 @@
+// Resultを取り扱う関数群
 module Result =
+    // A -> Result<T,E> である関数を Result<A,E> -> Result<T,E> に変換する
+    // ->引数がOkであった場合、中身をfに渡し、結果をそのまま返却
+    // ->引数がErrorであった場合、そのErrorをそのまま返却
     let bind f aResult =
         match aResult with
         | Ok success -> f success
         | Error failure -> Error failure
 
+    // A -> B である関数を Result<A,E> -> Result<B,E> に変換する
+    // ->引数がOkであった場合、中身をfに渡し、結果をOkでラップする
+    // ->引数がErrorであった場合、そのErrorをそのまま返却
     let map f aResult =
         match aResult with
         | Ok success -> Ok(f success)
         | Error failure -> Error failure
 
-    let mapError f aResult =
+    // ResultがErrorであるとき、別のError型に変換する
+    // ->引数がOkであった場合、中身をOkで再ラップして返却
+    // ->引数がErrorであった場合、中身をfに渡し、結果をErrorでラップして返却 この時のfは別のError型用のコンストラクタ
+    let mapError errorConstructor aResult =
         match aResult with
         | Ok success -> Ok success
-        | Error failure -> Error(f failure)
+        | Error failure -> Error(errorConstructor failure)
 
+// 単純型定義
 type Apple = Apple of string
 type Bananas = Bananas of string
 type Cherries = Cherries of string
 type Lemons = Lemons of string
-type AppleError = AppleError of string
-type BananaError = BananaError of string
 
+// 各ステップの関数とエラー型定義
 type FunctionA = Apple -> Result<Bananas, AppleError>
+and AppleError = AppleError of string
 type FunctionB = Bananas -> Result<Cherries, BananaError>
+and BananaError = BananaError of string
 type FunctionC = Cherries -> Lemons
 
+// ワークフロー全体のエラー型定義
 type FruitError =
     | AppleErrorCase of AppleError
     | BananaErrorCase of BananaError
@@ -44,22 +57,23 @@ let functionB: FunctionB =
 
 let functionC: FunctionC = fun _cherries -> Lemons "Lemons"
 
-let functionAWithFruitError input =
-    input |> functionA |> Result.mapError AppleErrorCase
-
-let functionBWithFruitError input =
-    input |> functionB |> Result.mapError BananaErrorCase
-
+// FunctionAのErrorをFruitErrorに変換
+let functionAWithFruitError = functionA >> Result.mapError AppleErrorCase
+// FunctionBのErrorをFruitErrorに変換
+let functionBWithFruitError = functionB >> Result.mapError BananaErrorCase
+// functionBWithFruitErrorの引数をResultに変換
 let functionBWithFruitErrorBound = Result.bind functionBWithFruitError
-
+// ABを結合
 let functionAB = functionAWithFruitError >> functionBWithFruitErrorBound
 
 printfn "%A" (functionAB (Apple "Banana"))
 printfn "%A" (functionAB (Apple "Apple"))
 printfn "%A" (functionAB (Apple "d"))
 
+// FunctionCの引数と戻り値をResultに変換
 let functionCMapped = Result.map functionC
 
+// ABCを結合
 let functionABC =
     functionAWithFruitError >> functionBWithFruitErrorBound >> functionCMapped
 
