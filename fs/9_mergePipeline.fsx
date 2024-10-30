000d5d9d63b362beb12d184afe668636378e7559
浅見 朋 <t-asami@techfirm.co.jp>
2024-10-30 17:54:15 +0900

    少し直す

--- fs/9_mergePipeline.fsx
+++ fs/9_mergePipeline.fsx
@@ -9,45 +9,57 @@ module Domain =
             if String.IsNullOrEmpty(str) then
                 // エフェクトを避ける為に例外を投げる
                 // 別にResult型を使ってもいい
-                failwith "OrderId must not be null or empty"
+                Error "OrderId must not be null or empty"
             elif str.Length > 50 then
-                failwith "OrderId must not be more than 50 chars"
+                Error "OrderId must not be more than 50 chars"
             else
-                OrderId str
+                Ok (OrderId str)
 
         let value (OrderId str) = str
 
 
 open Domain
+
 type Undefined = exn
+
 type AsyncResult<'success, 'failure> = Async<Result<'success, 'failure>>
+
+type ValidationError =
+    { FieldName: string
+      ErrorDescription: string }
+
 type UnvalidatedAddress = UnvalidatedAddress of string
 and OrderId = Undefined
 and CustomerInfo = Undefined
 and Address = Address of string
+
 type ValidatedOrderLine = ValidatedOrderLine of Undefined
 and UnvalidatedCustomer = { Name: string; Email: string }
+
 type UnvalidatedOrder =
     { OrderId: string
       CutomerInfo: UnvalidatedCustomer
       ShippingAddress: UnvalidatedAddress }
+
 type ValidatedOrder =
     { OrderId: OrderId
       CustomerInfo: CustomerInfo
       ShippingAddress: Address
       BillingAddress: Address
       OrderLine: ValidatedOrderLine list }
+
 type ProductCode =
     | WidgetCode of WidgetCode
     | GizmoCode of GizmoCode
 and WidgetCode = WidgetCode of string
 and GizmoCode = GizmoCode of string
-type CheckProductCodeExists = ProductCod
+
+type CheckProductCodeExists = string -> Result<ProductCode, ValidationError>
+
 type CheckAddress = CheckAddress of Address
+
 type PlacedOrderError = ValidationError of ValidationError list
-and ValidationError =
-    { FieldName: string
-      ErrorDescription: string }
+      
 type AddressValidationError = AddressValidationError of string
 
 // ------------- 型定義ここまで
